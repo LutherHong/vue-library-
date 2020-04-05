0a1495a08473abef56cc5ee550f49e3e58ea305a
zhaozhihong <346537985@qq.com>
2020-04-05 15:03:06 +0800

    chore: （十四）：完善的访问拦截

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,6 +10,8 @@
         <el-input type="password" v-model="loginForm.password"
                   auto-complete="off" placeholder="密码"></el-input>
       </el-form-item>
+      <el-checkbox class="login_remember" v-model="checked"
+                   label-position="left"><span style="color: #505458">记住密码</span></el-checkbox>
       <el-form-item style="width: 100%">
         <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
         <router-link to="register"><el-button type="primary" style="width: 40%;background: #505458;border: none">注册</el-button></router-link>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
+axios.defaults.withCredentials = true
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 
@@ -20,8 +21,10 @@ Vue.use(ElementUI)
 
 router.beforeEach((to,from,next)=>{
   if(to.meta.requireAuth){
-    if(store.state.user.username){
-      next()
+    if(store.state.user) {
+      axios.get('/authentication').then(resp => {
+        if (resp) next()
+      })
     }else{
       next({
         path:'login',
